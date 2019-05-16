cacba777a20d8d8a8428b26711a4d4739a9f1d79
Jeerel <zhatwo@gmail.com>
2019-05-16 01:05:58 -0500

    tabla autor
    creamos tabla autor

---
+++ proyecto.sql
@@ -0,0 +1,14 @@
+/*
+Universidad Nacional Autonoma de Mexico
+
+Proyecto de Base de Datos
+
+Base de datos de libreria 
+
+Integrantes:
+Herrejon Alarcon Jeerel Zalatiel
+*/
+
+CREATE TABLE autor(
+	id_autor
+);
