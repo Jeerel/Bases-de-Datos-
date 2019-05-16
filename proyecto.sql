64feadd3077f8de298a20e99ee7296474726d5d3
Daniel Hernández <47311620+DanielHdzGvr@users.noreply.github.com>
2019-05-16 02:15:02 -0500

    Update proyecto.sql

--- proyecto.sql
+++ proyecto.sql
@@ -7,6 +7,7 @@ Base de datos de libreria
 
 Integrantes:
 Herrejon Alarcon Jeerel Zalatiel
+Hernández Guevara Daniel Alberto
 */
 
 CREATE TABLE autor
@@ -48,6 +49,61 @@ CREATE TABLE tipoLect(
 	id_tipoLect CHAR(4) PRIMARY KEY,
 	id_lector CHAR(4) NOT NULL,
 	--tengo dudas en los siguientes atributos de esta tabla
+);
+
+CREATE TABLE ejemplar(
+	no_ejemplar NUMBER(4), 
+	id_material NUUMBER(4) NOT NULL,
+	status VARCHAR2(20) NOT NULL,
+	CONSTRAINT PkEjemplar PRIMARY KEY no_ejemplar,
+	CONSTRAINT FkEjemplar FOREIGN KEY id_material REFERENCES material,
+	CONSTRAINT Estado CHECK status IN ('Disponible','Prestamo','No sale','Mantenimiento'));
+	--Tengo dudas de la sintaxis de los constraints ↑
+	
+
+CREATE TABLE material(
+	id_material NUMBER(4),
+	titulo VARCHAR2(30) NOT NULL,
+	ubicacion VARCHAR2(30) NOT NULL,
+	colocacion VARCHAR2(30) NOT NULL,
+	CONSTRAINT PkMaterial PRIMARY KEY id_material
+	
+	
+);
+
+CREATE TABLE tesis(
+	id_tesis NUMBER(4),
+	id_material NUMBER(4),
+	carrera VARCHAR2(30) NOT NULL,
+	anio_pub VARCHAR2(30) NOT NULL,
+	CONSTRAINT FkTesis FOREIGN KEY id_material REFERENCES material,
+	CONSTRAINT PkTesis PRIMARY KEY id_tesis
+	
+);
+
+CREATE TABLE libro(
+	no_adqui NUMBER(4),
+	id_Material NUMBER(4),
+	ISBN NUMBER(13) NOT NULL,
+	edicion NUMBER(2) NOT NULL,
+	tema VARCHAR2(30) NOT NULL,
+	CONSTRAINT FkLibro FOREIGN KEY id_material REFERENCES material,
+	CONSTRAINT PkLibro PRIMARY KEY no_adqui
+
+	
+);
+
+CREATE TABLE director (
+	id_director NUMBER(4),
+	id_tesis NUMBER(4),
+	id_material NUMBER(4),
+	nomb_direct VARCHAR2(50) NOT NULL,
+	gdo_acad VARCHAR2(30) NOT NULL,
+	CONSTRAINT FkTesis FOREIGN KEY id_tesis REFERENCES tesis,
+	CONSTRAINT FkMaterial FOREIGN KEY id_material REFERENCES material,
+	CONSTRAINT PkDirector PRIMARY KEY (id_director, id_tesis, id_material)
+	
+	
 );
 
 
