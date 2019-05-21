1beb2afdb04df634ec11d018873e59e990fc5460
Daniel Hernández <47311620+DanielHdzGvr@users.noreply.github.com>
2019-05-20 23:13:36 -0500

    Update Procedimientos_vistas_y_lo_que_haga_falta.sql

--- Procedimientos_vistas_y_lo_que_haga_falta.sql
+++ Procedimientos_vistas_y_lo_que_haga_falta.sql
@@ -1,3 +1,5 @@
+--Dany estuvo aquí Ü y seguramente los salvará para que aprueben la materia de BD.
+
 --Disparadoror para actualizar estado de prestamo
 CREATE OR REPLACE TRIGGER registra_prestamo
 AFTER INSERT
@@ -21,3 +23,29 @@ WHERE no_ejemplar = :OLD.no_ejemplar;
 DBMS_OUTPUT.PUT_LINE('Ejemplar devuelto');
 END;
 /
+
+--Procedimiento para realizar un préstamo 
+
+CREATE OR REPLACE PROCEDURE prestamo(
+vidLect lector.id_lector%TYPE,
+vnumEjem ejemplar.no_ejemplar%TYPE
+)
+AS
+multa NUMBER(3);
+Prof tipoLector.profesor%TYPE;
+Alumn tipoLector.alumno%TYPE;
+Inves tipoLector.investigador%TYPE;
+Fecha_dev DATE;
+BEGIN
+SELECT profesor, alumno, investigador INTO Prof,Alumn,Inves
+FROM tipo_lector WHERE id_lector=vidLect;
+multa:=fmulta(vidLect);
+IF (multa>0) THEN
+DBMS_OUTPUT.PUT_LINE('El usuario presentaun adeudo');
+ELSE
+Fecha_dev := ffechaDe(Prof,Alumn,Inves);
+INSERT INTO prestamo VALUES('P'||prestamoSec.NEXTVAL,vnumEjem,SYSDATE,Fecha_dev,'0',vidLect);
+END IF;
+END;
+/
+
